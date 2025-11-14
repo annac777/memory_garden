5096a95dcb0175c857dba139b6b80d4cd02d1845
annac <xc2736@nyu.edu>
2025-11-13 19:46:32 -0500

    new models

--- src/addDefaultMeshes.js
+++ src/addDefaultMeshes.js
@@ -2,7 +2,7 @@ import * as Three from 'three'
 
 export const addDefaultMeshes = ({ xPoz = 0, yPoz = 0, zPoz = 0 } = {}) => {
     const geometry = new Three.BoxGeometry()
-    const material = new Three.MeshBasicMaterial({ color: 'ffffff' })
+    const material = new Three.MeshBasicMaterial({ color: '#ffffff' })
     const mesh = new Three.Mesh(geometry, material)
     mesh.position.set(xPoz, yPoz, zPoz)
     return mesh
@@ -10,7 +10,7 @@ export const addDefaultMeshes = ({ xPoz = 0, yPoz = 0, zPoz = 0 } = {}) => {
 
 export const addStandardMeshes = ({ xPoz = 0, yPoz = 0, zPoz = 0 } = {}) => {
     const geometry = new Three.BoxGeometry(1, 1, 1)
-    const material = new Three.MeshStandardMaterial({ color: 'ffffff' })
+    const material = new Three.MeshStandardMaterial({ color: '#ffffff' })
     const mesh = new Three.Mesh(geometry, material)
     mesh.position.set(xPoz, yPoz, zPoz)
     return mesh
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { addDefaultMeshes, addStandardMeshes } from './addDefaultMeshes'
 import { addLight } from './addLight'
-import { add } from 'three/tsl'
+import Model from './model'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -10,6 +10,8 @@ camera.position.set(0, 0, 5)
 const renderer = new THREE.WebGLRenderer({ antialias: true })
 const meshes = {}
 const lights = {}
+const mixers = []
+const clock = new THREE.Clock()
 init()
 
 function init() {
@@ -26,10 +28,29 @@ function init() {
   scene.add(meshes.standard)
   scene.add(lights.dafault)
 
+  instances()
   animate()
 }
 
+function instances() {
+  const flowers = new Model({
+    url: 'flowers.glb',
+    name: 'flower',
+    scene: scene,
+    meshes: meshes,
+    scale: new THREE.Vector3(2, 2, 2),
+    position: new THREE.Vector3(0, -0.8, 3),
+    animationState: true,
+    mixers: mixers,
+  })
+  flowers.init()
+}
+
 function animate() {
+  const delta = clock.getDelta()
+  for (const mixer of mixers) {
+    mixer.update(delta)
+  }
   meshes.standard.rotation.x += 0.01
   meshes.standard.rotation.y += 0.01
   meshes.default.rotation.x += 0.01
@@ -37,6 +58,3 @@ function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
 }
-
-
-
